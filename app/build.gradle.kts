d652d97e1229c79e4d09fd04cfa6bb088d913ea5
Arsalan Khan <arsalanbinkhan@gmail.com>
2025-07-26 22:35:32 +0530

    Arsalan And Zain Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.material:material:1.x.x")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
