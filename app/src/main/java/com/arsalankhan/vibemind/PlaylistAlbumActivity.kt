7e70ccebffdad7e3c03a706a6a05c166c6dea370
Arsalan Khan <arsalanbinkhan@gmail.com>
2025-07-27 18:41:17 +0530

    Arsalan Commit Ui Complete

--- app/src/main/java/com/arsalankhan/vibemind/PlaylistAlbumActivity.kt
+++ app/src/main/java/com/arsalankhan/vibemind/PlaylistAlbumActivity.kt
@@ -1,5 +1,7 @@
 package com.arsalankhan.vibemind
 
+import android.app.ActivityOptions
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.arsalankhan.vibemind.databinding.PlaylistAlbumBinding
@@ -12,7 +14,11 @@ class PlaylistAlbumActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = PlaylistAlbumBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        binding.btnBack.setOnClickListener {
+            val intent= Intent(this, MainActivity::class.java)
+            val options = ActivityOptions.makeCustomAnimation(this, R.anim.fade_in, R.anim.fade_out)
+            startActivity(intent, options.toBundle())
+        }
 
     }
 }
--- app/src/main/java/com/arsalankhan/vibemind/SettingsActivity.kt
+++ app/src/main/java/com/arsalankhan/vibemind/SettingsActivity.kt
@@ -1,5 +1,7 @@
 package com.arsalankhan.vibemind
 
+import android.app.ActivityOptions
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.arsalankhan.vibemind.databinding.ActivitySettingsBinding
@@ -12,6 +14,11 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.btnBack.setOnClickListener {
+            val intent= Intent(this, MainActivity::class.java)
+            val options = ActivityOptions.makeCustomAnimation(this, R.anim.fade_in, R.anim.fade_out)
+            startActivity(intent, options.toBundle())
+        }
 
     }
 }
